8c54b802d130b6025222d3c4d864a5e80c2ae3ea
biosfood <leisenhau@gmail.com>
2024-02-02 12:31:48 +0100

    judge article results by probable usefulness

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ When searching for aritcles, it is important to only use few words and just incl
 Istruct: {question}
 Output: Relevant articles to answer this question can be found by the following query: \""""
 
-question = "How much coffee can I consume in a day?"
+question = "How much coffee can I consume in a day without fearing negative health outcomes?"
 
 keyword_chain = PromptTemplate.from_template(template) | llm
 
@@ -63,11 +63,19 @@ client = arxiv.Client()
 
 search = arxiv.Search(
   query = query,
-  max_results = 3,
+  max_results = 10,
   sort_by = arxiv.SortCriterion.Relevance
 )
 
 results = client.results(search)
 
+template = """The assistant is a state of the art assistant but has not received recent updates. However, the assistant is allowed to search for articles to form a well researched anwser to its question.
+After searching for articles matching the query, we will now have to judge them on their likelihood of being helpful.
+Istruct: You are trying to answer the question: \"{question}\". In an effort to answer this, you have searched for articles using the query \"{query}\". One specifc article we found has the title \"{title}\". Will this article be useful and worth investigating further.
+Output: On a scale from 1 to 10, the likelyhood of this article helping me answer the question \"{question}\" is: \""""
+
+value_chain = PromptTemplate.from_template(template) | llm
+
 for result in results:
-    print(result)
+    value = value_chain.invoke({"question": question, "query": query, "title": result.title}).split("\"")[0]
+    print(f"\"{result.title}\": {value}")
