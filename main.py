91b774ea0b3afad92df8d04c95d9fd077ded7c94
biosfood <leisenhau@gmail.com>
2024-02-03 23:05:27 +0100

    include CORE API for querying articles

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from langchain.prompts import PromptTemplate
 import arxiv
 client = arxiv.Client()
 
+import requests
 
 template = """The assistant is a state of the art assistant but has not received recent updates. However, the assistant is allowed to search for articles to form a well researched anwser to its question.
 When searching for aritcles, it is important to only use few words and just include relevant keywords. Some examples for good queries are: "transformer language model architecture" or "methanol consumption effects".
@@ -19,12 +20,27 @@ query = keyword_chain.invoke({"question": question}).split("\"")[0]
 
 print(f"seaching with keyword \"{query}\"")
 
+results = []
+
 search = arxiv.Search(
   query = query,
-  max_results = 100,
+  max_results = 20,
   sort_by = arxiv.SortCriterion.Relevance
 )
-results = client.results(search)
+arxiv_results = client.results(search)
+
+for result in arxiv_results:
+    results.append({"title": result.title, "summary": result.summary})
+
+payload = {
+    "q": query,
+    "sort": "relevance"
+}
+
+result = requests.get("https://api.core.ac.uk/v3/search/works", params=payload)
+print(result.url)
+for result in result.json()["results"]:
+    results.append(result)
 
 template = """The assistant is a state of the art assistant but has not received recent updates. However, the assistant is allowed to search for articles to form a well researched anwser to its question.
 After searching for articles matching the query, we will now have to judge them on their likelihood of being helpful.
@@ -34,5 +50,6 @@ Output: On a scale from 1 to 10, the likelyhood of this article helping me answe
 value_chain = PromptTemplate.from_template(template) | llm
 
 for result in results:
-    value = value_chain.invoke({"question": question, "query": query, "title": result.title}).split("\"")[0]
-    print(f"\"{result.title}\": {value}")
+    title = result["title"]
+    value = value_chain.invoke({"question": question, "query": query, "title": title}).split("\"")[0]
+    print(f"\"{title}\": {value}")
