9b39300f2995fe78a80e9a9c5054d2f1d3e485b8
biosfood <leisenhau@gmail.com>
2024-02-07 15:49:14 +0100

    fix warning by huggingface about incompatible types of Model

--- model.py
+++ model.py
@@ -11,6 +11,8 @@ from intel_extension_for_transformers.transformers.pipeline import pipeline
 woq_config = WeightOnlyQuantConfig(weight_dtype="int4", compute_dtype="int8")
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=woq_config,trust_remote_code=True)
 
+model.__class__.__name__ = "PhiForCausalLM"
+
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 streamer = TextStreamer(tokenizer, skip_prompt=False)
 
